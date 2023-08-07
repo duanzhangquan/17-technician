c15b9ee02b5a8cacf9a1bb24d4993fed93cb2d86
duanzhangquan <691240951@qq.com>
2023-08-08 04:00:14 +0800

    修改有语法错误的github actions文件(第11次修改)

--- doc/docker-compose/bin/update.sh
+++ doc/docker-compose/bin/update.sh
@@ -6,25 +6,26 @@ echo '=====开始关闭运行的容器====='
 sh kernShutdown.sh
 
 echo '=====开始更新mogu-gateway====='
-docker pull registry.cn-guangzhou.aliyuncs.com/junmowen-repo/mogu_gateway:latest
+# --authfile ~/.docker/config.json是指使用root目录下的.docker/config.json文件进行免密登录
+docker pull registry.cn-guangzhou.aliyuncs.com/junmowen-repo/mogu_gateway:latest --authfile ~/.docker/config.json
 
 echo '=====开始更新mogu-admin====='
-docker pull registry.cn-guangzhou.aliyuncs.com/junmowen-repo/mogu_admin:latest
+docker pull registry.cn-guangzhou.aliyuncs.com/junmowen-repo/mogu_admin:latest --authfile ~/.docker/config.json
 
 echo '=====开始更新mogu-web====='
-docker pull registry.cn-guangzhou.aliyuncs.com/junmowen-repo/mogu_web:latest
+docker pull registry.cn-guangzhou.aliyuncs.com/junmowen-repo/mogu_web:latest --authfile ~/.docker/config.json
 
 echo '=====开始更新mogu-sms====='
-docker pull registry.cn-guangzhou.aliyuncs.com/junmowen-repo/mogu_sms:latest
+docker pull registry.cn-guangzhou.aliyuncs.com/junmowen-repo/mogu_sms:latest --authfile ~/.docker/config.json
 
 echo '=====开始更新mogu-picture====='
-docker pull registry.cn-guangzhou.aliyuncs.com/junmowen-repo/mogu_picture:latest
+docker pull registry.cn-guangzhou.aliyuncs.com/junmowen-repo/mogu_picture:latest --authfile ~/.docker/config.json
 
 echo '=====开始更新vue_mogu_admin====='
-docker pull registry.cn-guangzhou.aliyuncs.com/junmowen-repo/vue_mogu_admin:latest
+docker pull registry.cn-guangzhou.aliyuncs.com/junmowen-repo/vue_mogu_admin:latest --authfile ~/.docker/config.json
 
 echo '=====开始更新vue_mogu_web====='
-docker pull registry.cn-guangzhou.aliyuncs.com/junmowen-repo/vue_mogu_web:latest
+docker pull registry.cn-guangzhou.aliyuncs.com/junmowen-repo/vue_mogu_web:latest --authfile ~/.docker/config.json
 
 echo '=====删除docker标签为none的镜像====='
 docker images | grep none | awk '{print $3}' | xargs docker rmi
