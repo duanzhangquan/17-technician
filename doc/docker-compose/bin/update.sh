0e55759c89ea30cf9ed1c6dbf3a7d93dc6faee15
duanzhangquan <691240951@qq.com>
2023-08-08 02:44:13 +0800

    修改有语法错误的github actions文件(第10次修改)

--- doc/docker-compose/bin/update.sh
+++ doc/docker-compose/bin/update.sh
@@ -6,28 +6,31 @@ echo '=====开始关闭运行的容器====='
 sh kernShutdown.sh
 
 echo '=====开始更新mogu-gateway====='
-docker pull registry.cn-guangzhou.aliyuncs.com/junmowen-repo/mogu_gateway
+docker pull registry.cn-guangzhou.aliyuncs.com/junmowen-repo/mogu_gateway:latest
 
 echo '=====开始更新mogu-admin====='
-docker pull registry.cn-guangzhou.aliyuncs.com/junmowen-repo/mogu_admin
+docker pull registry.cn-guangzhou.aliyuncs.com/junmowen-repo/mogu_admin:latest
 
 echo '=====开始更新mogu-web====='
-docker pull registry.cn-guangzhou.aliyuncs.com/junmowen-repo/mogu_web
+docker pull registry.cn-guangzhou.aliyuncs.com/junmowen-repo/mogu_web:latest
 
 echo '=====开始更新mogu-sms====='
-docker pull registry.cn-guangzhou.aliyuncs.com/junmowen-repo/mogu_sms
+docker pull registry.cn-guangzhou.aliyuncs.com/junmowen-repo/mogu_sms:latest
 
 echo '=====开始更新mogu-picture====='
-docker pull registry.cn-guangzhou.aliyuncs.com/junmowen-repo/mogu_picture
+docker pull registry.cn-guangzhou.aliyuncs.com/junmowen-repo/mogu_picture:latest
 
 echo '=====开始更新vue_mogu_admin====='
-docker pull registry.cn-guangzhou.aliyuncs.com/junmowen-repo/vue_mogu_admin
+docker pull registry.cn-guangzhou.aliyuncs.com/junmowen-repo/vue_mogu_admin:latest
 
 echo '=====开始更新vue_mogu_web====='
-docker pull registry.cn-guangzhou.aliyuncs.com/junmowen-repo/vue_mogu_web
+docker pull registry.cn-guangzhou.aliyuncs.com/junmowen-repo/vue_mogu_web:latest
 
 echo '=====删除docker标签为none的镜像====='
 docker images | grep none | awk '{print $3}' | xargs docker rmi
 
 echo '=====开始运行的一键部署脚本====='
 sh kernStartup.sh
+
+echo '=====镜像更新完成====='
+echo '=====服务重启完成====='
